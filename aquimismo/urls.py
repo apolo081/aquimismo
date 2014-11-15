9b2e1c1ec3b988893f748eeded6f8e9b9788d8f6
Luis Moncaris <lmoncarisg@gmail.com>
2014-11-15 11:42:14 -0500

    * Agregado permisos a los comentarios * Eliminada posibilidad de ver comentarios por separado. * Solo el usuario creador puede modificar/borrar

--- aquimismo/urls.py
+++ aquimismo/urls.py
@@ -3,14 +3,10 @@ from django.contrib import admin
 from rest_framework import routers
 from comments.views import CommentViewSet
 
-router = routers.SimpleRouter()
-router.register(r'comment_users',CommentViewSet)
-print router.urls
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'aquimismo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^(?P<id_object>\d+)/',include(router.urls))
 )
--- comments/views.py
+++ comments/views.py
@@ -1,12 +1,17 @@
+import warnings
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
-from django.core.exceptions import ImproperlyConfigured
-from django.http.response import Http404
+from django.core.exceptions import ImproperlyConfigured, ObjectDoesNotExist
+from django.http.response import Http404, HttpResponseForbidden
 from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
+from rest_framework.exceptions import MethodNotAllowed
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.response import Response
 from comments.models import Comment
+from comments.permissions import IsCommentOwnerOrReadOnly
 from comments.serializers import CommentSerializer
 
 
@@ -16,6 +21,9 @@ class CommentViewSet(viewsets.ModelViewSet):
     model_for_comment = None
     serializer_class = CommentSerializer
     extra_args = {}
+    renderer_classes = (JSONRenderer,BrowsableAPIRenderer)
+    permission_classes = (IsCommentOwnerOrReadOnly,)
+
 
     def dispatch(self, request, id_object, *args, **kwargs):
         """
@@ -29,17 +37,18 @@ class CommentViewSet(viewsets.ModelViewSet):
         """
         if self.model_for_comment == None:
             raise ImproperlyConfigured(
-                    'Expected model_for_comment class field.'
+                    'Expected model_for_comment class field. '
                     'Please set up the model_for_comment first in your CommentViewSet'
                     'inherit class'
             )
 
-
         content_type = ContentType.objects.get_for_model(self.model_for_comment())
 
-        if not content_type.get_object_for_this_type(pk=id_object).exist():
-            raise Http404('The object you tried to comment do not exists')
 
+        try:
+            content_type.get_object_for_this_type(pk=id_object)
+        except ObjectDoesNotExist:
+            raise Http404('Object not found')
 
         self.extra_args['content_type'] = content_type
         self.extra_args['id_object'] = id_object
@@ -52,3 +61,30 @@ class CommentViewSet(viewsets.ModelViewSet):
         obj.user = self.extra_args.pop('user')
         super(CommentViewSet,self).pre_save(obj)
 
+    def list(self, request, *args, **kwargs):
+        self.object_list = self.filter_queryset(self.get_queryset())
+        self.object_list = self.object_list.filter(object_id=self.extra_args['id_object'])
+        # Default is to allow empty querysets.  This can be altered by setting
+        # `.allow_empty = False`, to raise 404 errors on empty querysets.
+        if not self.allow_empty and not self.object_list:
+            warnings.warn(
+                'The `allow_empty` parameter is deprecated. '
+                'To use `allow_empty=False` style behavior, You should override '
+                '`get_queryset()` and explicitly raise a 404 on empty querysets.',
+                DeprecationWarning
+            )
+            class_name = self.__class__.__name__
+            error_msg = self.empty_error % {'class_name': class_name}
+            raise Http404(error_msg)
+
+        # Switch between paginated or standard style responses
+        page = self.paginate_queryset(self.object_list)
+        if page is not None:
+            serializer = self.get_pagination_serializer(page)
+        else:
+            serializer = self.get_serializer(self.object_list, many=True)
+
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        raise MethodNotAllowed('retrieve')
